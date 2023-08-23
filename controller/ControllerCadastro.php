eda794acb4ed8799f5106973b3e5e1c80dfdbf3b
Tatiane Guzman <126589385+tat1546@users.noreply.github.com>
2023-08-22 21:38:27 -0300

    22_08

---
+++ controller/ControllerCadastro.php
@@ -0,0 +1,25 @@
+<?php
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+require_once("$root/projetoweb/model/cadastro.php");
+class ControllerCadastro{
+    private $cadastro;
+    public function __construct(){
+        $this->cadastro = new Cadastro();
+        $this->incluir();
+    }
+    private function incluir(){
+        $this->cadastro->setNome($_POST['txtNome']);
+        $this->cadastro->setTelefone($_POST['txtTelefone']);
+        $this->cadastro->setOrigem($_POST['txtOrigem']);
+        $this->cadastro->setData_contato(date('Y-m-d',strtotime($_POST['txtDataContato'])));
+        $this->cadastro->setObservacao($_POST['txtObservacao']);
+        $result = $this->cadastro->incluir();
+        if($result >= 1){
+            echo "<script>alert('Registro incluído com sucesso!');document.location='../index.php'</script>";
+        }else{
+            echo "<script>alert('Erro ao gravar registro!');</script>";
+        }
+    }
+}
+new ControllerCadastro();
+?>
